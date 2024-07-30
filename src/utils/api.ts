fd6fe808190fa821e5b442beddb310864a2e8f5e
Ryan Carniato <ryansolid@gmail.com>
2024-07-30 14:12:25 -0700

    change delays to be around request instead of the request

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,19 @@
-const API_ENDPOINT = 'https://meek-gumption-c04779.netlify.app/vacations';
+const API_ENDPOINT = "https://meek-gumption-c04779.netlify.app/vacations";
 
-type APIPart = 'reviews' | 'recommendations' | 'rooms';
+type APIPart = "reviews" | "recommendations" | "rooms";
 
-export const fetchAPI = (part: APIPart, delay: number) => fetch(API_ENDPOINT + '?part=' + part + '&delay=' + delay);
+async function withDelay<T>(promise: Promise<T>, delay: number): Promise<T> {
+  // Ensure we throw if this throws
+  const ret = await promise;
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(ret);
+    }, delay);
+  });
+}
+
+export const fetchAPI = (part: APIPart, delay: number) =>
+  withDelay(
+    fetch(API_ENDPOINT + "?part=" + part).then((res) => res.json()),
+    delay
+  );
